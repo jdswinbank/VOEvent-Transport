11fc020f13470e645f755deea73fdb261f7d033d
John Swinbank <swinbank@transientskp.org>
2012-01-06 13:59:14 +0100

    Measure time over which data is *received*

--- receiver.py
+++ receiver.py
@@ -1,4 +1,4 @@
-# VOEvent receiver.
+h VOEvent receiver.
 # John Swinbank, <swinbank@transientskp.org>, 2011-12.
 
 # Python standard library
@@ -29,9 +29,9 @@ class EventIndex(object):
     def receive_event(self, timestring):
         timestamp = datetime.datetime.strptime(timestring, "%Y-%m-%dT%H:%M:%S.%f")
         received_time = datetime.datetime.utcnow()
-        if not hasattr(self, "earliest") or timestamp < self.earliest:
+        if not hasattr(self, "earliest") or received_time < self.earliest:
             self.earliest = received_time
-        if not hasattr(self, "latest") or timestamp > self.latest:
+        if not hasattr(self, "latest") or received_time > self.latest:
             self.latest = received_time
         self.latencies.append((received_time - timestamp).total_seconds())
 
